daca084df9c5ba4e4860df95f2a0d9668fd0071e
Trenton Schulz <trenton@nr.no>
2023-09-04 10:40:17 +0200

    Change directories and save the output.

--- scripts/start_gimmick_client.sh
+++ scripts/start_gimmick_client.sh
@@ -2,4 +2,8 @@
 
 source ~/rosa/scripts/activate-py2zqm.sh
 
-/home/nao/gimmick/test/scripts/main.py
+# Make sure that the python encoding is utf-8 always.
+PYTHONIOENCODING=utf8
+export PYTHONIOENCODING
+
+cd /home/nao/gimmick/test/scripts && python2 -u ./main.py >& /home/nao/gimmick/test/scripts/client.log
